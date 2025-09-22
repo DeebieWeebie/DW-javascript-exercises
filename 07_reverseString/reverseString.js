f2fffb92628d6032c5fde411679fede3f51fdf6c
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-09-22 12:26:40 -0400

    Final Commit reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,16 +1,15 @@
 
 reverseString = function(string) {
-    let stringArr = string.split(' ');      // split the string on the space into 2 words into an array
-    console.log(stringArr.length);           //2 words are in the word array...n words long
-    console.log(stringArr);
-    const numWords = stringArr.length;
-    const reverseArr = stringArr
-    return reverseArr.map(item => item.split('').reverse().join(''));  // split the array words into comma separated letters within each element
+    // let stringNoSpace = string.split(' ');      // split the string on the space into 2 words into an array
+    // console.log(stringNoSpace.length);           //2 words are in the word array...n words long
+    //  console.log(stringNoSpace);
+    // const reverseString = stringNoSpace
+    return string.split('').reverse().join('');  // split the array words into comma separated letters within each element
                                                                         // then reverse the letters within each element
                                                                         // then join the letters without the commas
 };
 
-console.log(reverseString('hello there'));
+console.log(reverseString(''));
 
 // Do not edit below this line
 module.exports = reverseString;
