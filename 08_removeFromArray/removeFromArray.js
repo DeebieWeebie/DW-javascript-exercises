22e7f1769e99b0fa3fd224ce5b4e781531d82ced
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-09-22 19:11:02 -0400

    Final commit removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,19 +1,19 @@
 const removeFromArray = function(arr, ...manyMoreArgs) {
-    const valuesToDelete = [...manyMoreArgs].filter(item => typeof item === 'number');
+    const valuesToDelete = [...manyMoreArgs];    //.filter(item => typeof item === 'number');
     let newArray = [...arr];
-    console.log(valuesToDelete);
+    console.log(`Vals to delete: ${valuesToDelete}`);
 
     for (let i = newArray.length -1; i >= 0; i--) {
         if (valuesToDelete.includes(newArray[i])) {
             newArray = newArray.toSpliced(i, 1);
         }
     }   
-    
-    console.log(newArray);
+
+    console.log(`The returned array is: ${newArray}`);
     return (newArray);
 };
 
-removeFromArray([1, 2, 3, 4], 7, "tacos"); // added a filter to valuesToDelete for numbers only
+removeFromArray([1, 2, 3], '1', 3);
                                     
 // Do not edit below this line
 module.exports = removeFromArray;
