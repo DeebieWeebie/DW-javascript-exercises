63b1f1b7bf704a14c01e3c12f8ca59c8c850b2ce
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-09-23 13:24:19 -0400

    Initial Commit tested on console

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(inputA, inputB) {
+    let sum = 0;
+    if  (inputB > inputA) {
+        for (i = inputA; i <= inputB; i++) {
+            sum += i;
+        }
+    } else if (inputA > inputB) {
+        for (i = inputB; i <= inputA; i++) {
+            sum += i;
+        }
+    } else {
+        console.log("ERROR");
+    }
+    
+    console.log(sum);
+    return sum;
 };
+
+sumAll(2,4);
 
 // Do not edit below this line
 module.exports = sumAll;
