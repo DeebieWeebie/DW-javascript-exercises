fd155e6ccdf1bf9f349b2c01c7289bd299c3026e
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-09-23 15:49:44 -0400

    add conditions for integer & non neg values

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,22 +1,25 @@
 const sumAll = function(inputA, inputB) {
     let sum = 0;
-    if  (inputB > inputA) {
+
+    if (!Number.isInteger(inputA) && !Number.isInteger(inputB) && inputA < 0 && inputB < 0) {
+        return 'ERROR';
+    }
+
+    if (inputB > inputA) {
         for (i = inputA; i <= inputB; i++) {
             sum += i;
         }
     } else if (inputA > inputB) {
         for (i = inputB; i <= inputA; i++) {
             sum += i;
-        }
-    } else {
-        console.log("ERROR");
+        }  
     }
-    
+
     console.log(sum);
     return sum;
 };
 
-sumAll(2,4);
+sumAll(123, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
