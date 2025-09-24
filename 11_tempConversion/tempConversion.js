f53fd69b7daa4cde2b9029c7a0f45b026208eeab
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-09-24 14:05:29 -0400

    Final commit tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,14 +1,15 @@
 const convertToCelsius = function(tempF) {
-  degreeCelsius = (tempF - 32) * 5/9
+  degreeCelsius = (tempF - 32) * 5/9;
   return Math.round(degreeCelsius * 10) / 10;
 };
 
 const convertToFahrenheit = function(tempC) {
-  return (tempC * 9/5) + 32
+  degreeFahrenheit = (tempC * 9/5) + 32;
+  return Math.round(degreeFahrenheit * 10) / 10;
 };
 
-convertToCelsius(32);
-convertToFahrenheit();
+convertToCelsius();
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {
