cdc9462769d2f355f7358e07212d19ba128d6315
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-09-24 12:11:38 -0400

    Initial leapYear fn commit

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear = 0;
+    let userMessage = 'false';
+    do {
+        if (year % 100 === 0 && year % 400 === 0) {
+            const leapYear = year;
+            return userMessage = 'true';
 
+        } else if (year % 100 !== 0) { 
+            const leapYear = year;
+            return userMessage = 'true';
+        } else {
+            return userMessage = 'false'
+        }
+
+    } while (year % 4);
 };
+
+leapYears(1600);
 
 // Do not edit below this line
 module.exports = leapYears;
