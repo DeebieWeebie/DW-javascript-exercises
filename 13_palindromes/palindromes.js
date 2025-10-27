7ad85f7ca62260e684f551afa108f4c7f6fc9754
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-10-27 15:39:14 -0400

    Initial Commit single word palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reverseString = string.split('').reverse().join('');    // split the word into single letters within each element of an array
+                                                                // then reverse the letters within each element
+                                                                // then join the letters
+    if (string == reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
