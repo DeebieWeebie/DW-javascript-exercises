82dd2b81163424bc46ed0f7236930bdea5c17e65
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-10-27 14:35:33 -0400

    final commit calculator JS exercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,61 @@
-const add = function() {
-	
-};
 
-const subtract = function() {
-	
-};
+const add = function (a, b) {
+  let sum = a + b;
+  return sum;
+}
 
-const sum = function() {
-	
-};
+add(0, 0);
+add(2, 2);
+add(2, 6);
 
-const multiply = function() {
+const subtract = function (a, b) {
+  let diff = a - b;;
+  return diff;
+}
 
-};
+subtract(10, 4);
+subtract(-10, -4);
+subtract(-8, 7);
 
-const power = function() {
-	
-};
+const sum = function (inputArray) {
+  let sum = inputArray.reduce((acc, curr) => acc + curr, 0);
+  return sum;
+}
 
-const factorial = function() {
-	
-};
+sum([]);
+sum([7]);
+sum([7, 11]);
+sum([1, 3, 5, 7, 9]);
+
+const multiply = function (inputArray) {
+  let product = inputArray.reduce((acc, curr) => acc * curr);
+  return product;
+}
+
+multiply([2, 4]);
+multiply([2, 4, 6, 8, 10, 12, 14]);
+
+const power = function (a, b) {
+  let result = Math.pow(a, b);
+  return result;
+}
+
+power(4, 3);
+power(3, 10);
+
+const factorial = function (n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+factorial(0);
+factorial(1);
+factorial(2);
+factorial(5);
+factorial(10);
 
 // Do not edit below this line
 module.exports = {
