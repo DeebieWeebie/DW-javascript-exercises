6b42034d7ba0472e0027e328f66ac974c80ca65b
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-10-27 17:27:13 -0400

    Add space removal for multiple words

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,12 +1,15 @@
 const palindromes = function (string) {
     const cleanString = string.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g,'');
-    const reverseString = cleanString.split('').reverse().join('');    
-    return cleanString == reverseString;
+    const noSpaceString = cleanString.replace(/ /g, "");
+    const reverseString = noSpaceString.split('').reverse().join('');    
+    return noSpaceString == reverseString;
 };
 
 palindromes("racecar");
 palindromes("racecar!");
 palindromes("Racecar!");
+palindromes("A car, a man, a maraca.");
+palindromes("Animal loots foliated detail of stool lamina.");
 
 // Do not edit below this line
 module.exports = palindromes;
