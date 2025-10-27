37ab3b2c939d1eea7ae22f52e37a82e6ca86c800
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-10-27 17:58:15 -0400

    Works with evenly spaced numbers

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (string) {
-    const cleanString = string.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g,'');
-    const noSpaceString = cleanString.replace(/ /g, "");
+    const cleanString = string.toLowerCase().replace(/[^a-z]/g, '');  //replace that is NOT a letter
+    const noSpaceString = cleanString.replace(/ /g, '');
     const reverseString = noSpaceString.split('').reverse().join('');    
     return noSpaceString == reverseString;
 };
@@ -10,6 +10,9 @@ palindromes("racecar!");
 palindromes("Racecar!");
 palindromes("A car, a man, a maraca.");
 palindromes("Animal loots foliated detail of stool lamina.");
+palindromes("ZZZZ car, a man, a maracaz.");
+palindromes("rac3e3car");
+palindromes("r3ace3car");
 
 // Do not edit below this line
 module.exports = palindromes;
