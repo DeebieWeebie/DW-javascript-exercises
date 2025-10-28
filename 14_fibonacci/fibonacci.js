bc02e342c930b9d87c8a0c0acda2f8a846adc6ab
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-10-28 13:37:58 -0400

    Initial commit before testing first spec.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibArray = [1,1];
+        for (let i = 2; i < num; i++) {
+            let nextVal = fibArray[i-2] + fibArray[i-1];
+            fibArray.push(nextVal);
+        }
+    return fibArray[num];
 };
+
+fibonacci(3);
 
 // Do not edit below this line
 module.exports = fibonacci;
