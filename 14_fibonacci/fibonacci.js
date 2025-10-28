fc9c4cf053daef22eb64dfa5ff7160419e7382e4
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-10-28 14:14:29 -0400

    Add conditions for 0th, neg vals, & existing vals

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,13 +1,26 @@
 const fibonacci = function(num) {
     let fibArray = [1,1];
-        for (let i = 2; i < num; i++) {
-            let nextVal = fibArray[i-2] + fibArray[i-1];
-            fibArray.push(nextVal);
+        if (num > 2) {
+            for (let i = 2; i < num; i++) {
+                fibArray[i] = fibArray[i-2] + fibArray[i-1];
+                fibArray.push(fibArray[i]);
+                return fibArray[num]
+            }
+        } else if (num = 0) {
+            return 0;
+        } else if (num < 0) {
+            return "OOPS";
+        } else {
+            return fibArray[(num - 1)];
         }
     return fibArray[num];
 };
 
 fibonacci(3);
+fibonacci(6);
+fibonacci(10);
+fibonacci(15);
+fibonacci(25);
 
 // Do not edit below this line
 module.exports = fibonacci;
