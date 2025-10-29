8e66f29a899d109b82d3332698f51aa8abf3092e
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-10-29 12:57:25 -0400

    Add num conditions for negative, string, zero & 1

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,19 +1,22 @@
 const fibonacci = function(num) {
     let fibArray = [1,1];
-        if (num > 2) {
-            for (let i = 2; i < num; i++) {
+    integer = parseInt(num);
+
+        if (integer >= 2) {
+            for (let i = 2; i < integer; i++) {
                 fibArray[i] = fibArray[i-2] + fibArray[i-1];
                 fibArray.push(fibArray[i]);
-                return fibArray[num]
             }
-        } else if (num = 0) {
-            return 0;
-        } else if (num < 0) {
-            return "OOPS";
-        } else {
-            return fibArray[(num - 1)];
+            return fibArray[integer];
+        } else if (integer == 0) {
+            let zeroVal = 0;
+            return zeroVal;
+        } else if (integer < 0) {
+            let userMessage = "OOPS";
+            return userMessage;
+        } else if (integer == 1) {
+            return fibArray[1];
         }
-    return fibArray[num];
 };
 
 fibonacci(3);
@@ -21,6 +24,10 @@ fibonacci(6);
 fibonacci(10);
 fibonacci(15);
 fibonacci(25);
+fibonacci(0);
+fibonacci(-25);
+fibonacci("0");
+fibonacci("1");
 
 // Do not edit below this line
-module.exports = fibonacci;
+// module.exports = fibonacci;
