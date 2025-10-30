fcfbef7d4406fb49e5391b424f964ffcc1549e61
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-10-30 15:43:26 -0400

    Add calculated age key.  Find maxAge & filter obj

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
 
+const findTheOldest = function() {
+    people.forEach((element) => {
+        element.age = (this.yearOfDeath - this.yearOfBirth);
+    });
+
+    const maxAge = Math.max(...people.map(obj => obj.age));
+    return people.filter(obj => obj.age === maxAge);
 };
 
 // Do not edit below this line
