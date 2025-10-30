6658898d82680dec1a4ea443fe68d1da2f3ee56e
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-10-30 13:51:11 -0400

    Use .map to return all title keys to a new array
    .filter returns an array of objects based on a
    condition (not extracting all key values of type)
    
    while .map extracts all values of a specific key
    and returns a new array.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
 
+const getTheTitles = function() {
+    let titles = books.map(item => item.title);
+    return titles;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+//NOTE TO SELF
+//.filter returns an array of objects based on a condition (not extracting all
+// key values of type)
+//.map extracts all values of a specific key to a new array.  
