c3ecb8913522f2755b0e7224d91b36eaf26312ca
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-11-27 16:07:19 -0500

    First attempt at findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,30 +1,34 @@
-//NOTE does reduce loop though the array objects and compare ages?
-// review reduce, map.  Also, find tutorial on using array functions
-// on arrays with objects.  Also, I should have exercises from 
-// SOMEWHERE using accumulators.  
+  const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
-const people = [
-      {
-        name: "Carly",
-        yearOfBirth: 1942,
-        yearOfDeath: 1970,
-      },
-      {
-        name: "Ray",
-        yearOfBirth: 1962,
-        yearOfDeath: 2011,
-      },
-      {
-        name: "Jane",
-        yearOfBirth: 1912,
-        yearOfDeath: 1941,
-      },
-    ]
+  const findTheOldest = function() {
+    const peopleWithAges = people.map(person => ({
+        ...person, age: person.yearOfDeath - person.yearOfBirth
+    }));
 
-const findTheOldest = function() {
+    const maxAge = Math.max(...peopleWithAges.map(person => person.age));
+    const oldestPeople = peopleWithAges.filter(person => person.age === maxAge);
+    
+    return oldestPeople[0].name;
+  };
 
-
-};
-
+  const nameOfOldest = findTheOldest(people);
+  console.log(nameOfOldest);
+  
 // Do not edit below this line
 module.exports = findTheOldest;
